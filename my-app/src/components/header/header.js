2da0d40d48777d9978e4d46652dfdc98b9cb2c3a
djcorea <javier.corea27@gmail.com>
2021-03-03 16:04:26 -0600

    REACT

---
+++ my-app/src/components/header/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+export default class header extends Component {
+  render() {
+    return (
+        <>
+        {/* Header
+         ================================================== */}
+        <header id="home">   &lt;---Copy from this section
+          ...
+          ...
+        </header> {/* Header End */}
+        ...
+      </>
+        
+    )
+  }
+}
