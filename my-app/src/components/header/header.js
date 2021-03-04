dc31f1cea43c0418e38280a8d305dc3ed6ceeb50
djcorea <javier.corea27@gmail.com>
2021-03-03 23:36:24 -0600

    REACT PAGE

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -3,14 +3,26 @@ export default class header extends Component {
   render() {
     return (
         <>
-        {/* Header
-         ================================================== */}
-        <header id="home">   &lt;---Copy from this section
-          ...
-          ...
-        </header> {/* Header End */}
-        ...
-      </>
+{/* header
+    ================================================== */}
+<header className="s-header">
+  <div className="row s-header__nav-wrap">
+    <nav className="s-header__nav">
+      <ul>
+        <li className="current"><a className="smoothscroll" href="#hero">Home</a></li>
+        <li><a className="smoothscroll" href="#about">About</a></li>
+        <li><a className="smoothscroll" href="#resume">Resume</a></li>
+        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+        <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+        <li><a className="smoothscroll" href="#contact">Say Hello</a></li>
+      </ul>
+    </nav>
+  </div> {/* end row */}
+  <a className="s-header__menu-toggle" href="#0" title="Menu">
+    <span className="s-header__menu-icon" />
+  </a>
+</header> {/* end s-header */}
+</>
         
     )
   }
